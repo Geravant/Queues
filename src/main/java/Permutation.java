3646b44383148a9d83b3f7ad885a8720dffbd179
Geravant <igor.gera.gerasim@gmail.com>
2019-05-17 00:36:38 +0300

    Permutation implemented

---
+++ src/main/java/Permutation.java
@@ -0,0 +1,22 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.NoSuchElementException;
+
+public class Permutation {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue randomizedQueue = new RandomizedQueue<String>();
+        while (true) {
+            try {
+                randomizedQueue.enqueue(StdIn.readString());
+            }
+            catch (NoSuchElementException e) {
+                break;
+            }
+        }
+        for (int i = 0; i< k; i++) {
+            StdOut.println(randomizedQueue.dequeue());
+        }
+    }
+}
